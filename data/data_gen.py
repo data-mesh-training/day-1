528f1ab058a05167c688909e46d5b9df0d9989d8
mcs1408 <max.c.schultze@gmail.com>
2023-10-15 17:12:53 +0200

    increase readability of data generator

--- data/data_gen.py
+++ data/data_gen.py
@@ -42,10 +42,10 @@ def get_final_order_date(order_date):
 
 #non-empty order line
 def generate_order_line(order_id, line_number, order_date, country):
-  final_order_date = get_final_order_date(order_date) # random date between 2023-06-01 and 2023-08-31, can be empty in 5% cases
-  original_price = random.choice(PRICES) # random original prices between 5.00 and 50.00
-  discounted_price = round(original_price*random.choice(DISCOUNT_WEIGHTS),2) # actual price factoring in a random discount
-  earnings_after_taxes = round(discounted_price*0.81, 2) # earnings after deducting 19% taxes
+  final_order_date = get_final_order_date(order_date)                        # random date between 2023-06-01 and 2023-08-31, can be "null" in 5% cases
+  original_price = random.choice(PRICES)                                     # random original price between 5.00 and 50.00
+  discounted_price = round(original_price*random.choice(DISCOUNT_WEIGHTS),2) # actual price factoring in a random discount between 0 and 50%
+  earnings_after_taxes = round(discounted_price*0.81, 2)                     # earnings after deducting 19% taxes
   order_line = [[
     str(order_id),
     str(line_number),
@@ -56,15 +56,16 @@ def generate_order_line(order_id, line_number, order_date, country):
     str(earnings_after_taxes)]]
   return order_line
 
+#generation of given number of orders
 def generate_orders(number_of_orders):
   order_positions = []
   for order_id in range(1, number_of_orders+1):
-    line_numbers = random.choice(LINE_NUMBER_COUNT_WEIGHTS) # randomly select between 0 and 5 items per order. Orders can be empty!
-    if line_numbers == 0: # handling of empty orders
+    line_numbers = random.choice(LINE_NUMBER_COUNT_WEIGHTS)                  # randomly select between 0 and 5 items per order. Orders can be empty!
+    if line_numbers == 0:                                                    # handling of empty orders
       order_positions += [[str(order_id), "", "", "", "0.00", "0.00", "0.00"]]
     else:
-      order_date = START_DATE + datetime.timedelta(random.randint(0,91)) # random date between 2023-06-01 and 2023-08-31
-      country = random.choice(COUNTRIES) # randomly selected country of order placement
+      order_date = START_DATE + datetime.timedelta(random.randint(0,91))     # random order date between 2023-06-01 and 2023-08-31
+      country = random.choice(COUNTRIES)                                     # randomly selected country of order placement
       for line_number in range(1, line_numbers+1):
         order_positions += generate_order_line(order_id, line_number, order_date, country)
   return order_positions
@@ -82,6 +83,10 @@ with open('generated_data1.csv', 'w', newline='\n') as csvfile:
   for record in records:
     csvwriter.writerow(record)
 
+#-------------------------------------------------------------------------------
+#---------------- section for second dataset derived from first ----------------
+#-------------------------------------------------------------------------------
+
 INVERTED_PRICES = {str(value): key for key, value in PRICES.items()}
 REGION_COUNTRIES = {"DE": "DACH_DE", "AT": "DACH_AT", "CH": "DACH_CH", "BE": "BNL_BE", "NL": "BNL_NL", "FR": "MED_FR", "EN": "UK_EN", "DK": "NORD_DK", "FI": "NORD_FI", "IT": "MED_IT", "PL": "VIS_PL", "PT": "MED_PT", "ES": "MED_ES", "SE": "NORD_SE", "NO": "NORD_NO"}
 
@@ -89,11 +94,11 @@ def generate_new_order(order_line):
   if order_line[1] == "":
     return [[order_line[0], "", "", "", 0.00]]
   else:
-    order_line_id = order_line[0] + order_line[1]
-    article_id = INVERTED_PRICES[order_line[4]]
-    order_date = order_line[2]
-    country = REGION_COUNTRIES[order_line[3]]
-    sales_price = order_line[5]
+    order_line_id = order_line[0] + order_line[1] # combination of order_id and line_number into one field
+    article_id = INVERTED_PRICES[order_line[4]]   # new article_id is position of the price in the PRICES list
+    order_date = order_line[2]                    # order_date kept the same
+    country = REGION_COUNTRIES[order_line[3]]     # country adapted according to REGION_COUNTRIES mapping
+    sales_price = order_line[5]                   # sales_price is discounted_price before VAT
     new_order = [[
       order_line_id,
       str(article_id),
@@ -101,6 +106,10 @@ def generate_new_order(order_line):
       country,
       sales_price]]
     return new_order
+
+#-------------------------------------------------------------------------------
+#--------------- section for executing derivation and persisting ---------------
+#-------------------------------------------------------------------------------
 
 new_records = []
 for record in records:
